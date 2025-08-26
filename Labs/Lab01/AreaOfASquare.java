f624c80b3cd0cdf4db438006523070c155759dfc
Joyboy <mi@Mohammads-MacBook-Air.local>
2025-08-26 11:48:30 -0500

    Lab01

--- Labs/Lab01/AreaOfASquare.java
+++ Labs/Lab01/AreaOfASquare.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class AreaOfASquare {
 	public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter length of the square: ");
         int length = input.nextInt();
         int area = length * length;
-        System.out.println(area);
+        System.out.println("The area of theis sqaure is " + area );
 	}
 }
