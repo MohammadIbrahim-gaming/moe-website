5da6720e53648d32cd9c74e970cbf9b4517112f0
Joyboy <mi@Mohammads-MacBook-Air.local>
2025-09-24 23:34:17 -0500

    Lab03

--- Labs/Lab03/TriangleSideLengths.java
+++ Labs/Lab03/TriangleSideLengths.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TriangleType {
+public class TriangleSideLengths {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
